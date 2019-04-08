1acbbe69de843b5e374b4d88a57505dc110515a8
hugh52 <whcherry52@gmail.com>
2019-04-07 23:19:34 -0500

    Create 06_words_to_features.py

---
+++ nlp_sentiment/06_words_to_features.py
@@ -0,0 +1,46 @@
+
+
+# words to features
+
+# here we build off previous section and compile feature lists of words from positive reviews and words from negative reviews;
+# the idea is to hopefully see some trends or patterns in specific types of words from positive or negative reviews
+
+import nltk
+import random
+from nltk.corpus import movie_reviews
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
