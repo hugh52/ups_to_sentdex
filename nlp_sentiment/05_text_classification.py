20b5570c32a2f687860c46e5efe38a70fa3aff4c
hugh52 <whcherry52@gmail.com>
2019-04-07 21:46:05 -0500

    Update 05_text_classification.py

--- nlp_sentiment/05_text_classification.py
+++ nlp_sentiment/05_text_classification.py
@@ -2,7 +2,7 @@
 
 # text classification
 
-# This is a very broad subject; examples include classifying text as to whether it is about politics or about the military, or classifying
+# This is a very broad subject; examples include classifying text as to whether it is about politics, about the military, or classifying
 # based on the gender of the author who wrote the text; identifying if a body of text is spam or not spam for email filtering is a well
 # known classification task; our ultimate goal is to create a sentiment analysis algorithm.
 
@@ -15,9 +15,38 @@ import random
 from nltk.corpus import movie_reviews
 
 
-# Below, we are basically saying, in reference to the imported 'movie_reviews' dataset, in each category (positive or negative), take all
-# of the file IDs (each review has its own ID), then store the 'word_tokenized' version for the file ID (which will be a list of words),
+# Below, we basically say, in reference to the imported 'movie_reviews' dataset, for each category (positive or negative), take all of 
+# the file IDs (each review has its own ID), then store the 'word_tokenized' version for the file ID (which will be a list of words),
 # and follow that by the 'positive' or 'negative' label, all in one big list.
+
+documents = [(list(movie_reviews.words(fileid)), category)
+             for category in movie_review.category()
+             for fileid in movie_review.fileids()]
+
+# since we will be training and testing, we now use random to shuffle documents;
+# if we did not, we could (or would) end up training on mostly all negatives and a few positives, then testing against all positives
+random.shuffle(documents)
+
+# to examine a "small" (relatively) amount of the data we are working with, we examine the first item in documents, 'documents[1]';
+# this is still a large list; the first element is a list of the words, second element in the "pos" or "neg" label
+print(documents[1])
+
+# now, to collect all words that we find, we use the frequency distribution to find the most common words; 
+# punctuation and words unimportant for our purposes like "the" or "a" will of course be at the top of the list. however, since we plan
+# on creating a list of thousands of words, this is not a big deal; the actual important words start to show soon.
+all_words = []
+for w in movie_reviews.words():
+	all_words.append(w.lower())
+	
+all_words = nltk.FreqDist(all_words)
+
+# now to find the most common 15 words:
+print(all_words.most_common(15))
+
+# to find the number of occurrences of some given word:
+print(all_words["stupid"])
+
+
 
 
 
