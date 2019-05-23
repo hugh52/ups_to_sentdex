14a7b6b80b6cf04f6961cb4d3e14131430902461
hugh52 <whcherry52@gmail.com>
2019-05-22 21:41:25 -0500

    Update 11_combining_algos.py

--- nlp_sentiment/11_combining_algos.py
+++ nlp_sentiment/11_combining_algos.py
@@ -1,8 +1,14 @@
+#===============================================================================================================================#
 
-#======================================================================================================================================#
+# section i: imports 
+# section ii:  simply a copy of the previous code without any comments
+# section iii: additional imports, building, and descriptions of VoteClassifier class
+# section iv: code from previous sections combined together
+
+#===============================================================================================================================#
+# section i
+
 import nltk
-import numpy as np
-import pandas as pd
 import random
 import pickle
 import sklearn
@@ -13,6 +19,8 @@ from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import SVC, LinearSVC, NuSVC
 
+#===============================================================================================================================#
+# section ii
 
 documents = [(list(movie_reviews.words(fileid)), category)
             for category in movie_reviews.categories()
@@ -54,8 +62,6 @@ BNB_classifier = SklearnClassifier(BernoulliNB())
 BNB_classifier.train(training_set)
 print("BernoulliNB accuracy percent:", (nltk.classify.accuracy(BNB_classifier, testing_set))*100)
 
-# as seen in the imports above, a few additional classification algos are brought in from sklearn for comparison purposes;
-# and maybe to see if they might be of any help when mixed with the two or three we are already using...
 
 LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
 LogisticRegression_classifier.train(training_set)
@@ -76,16 +82,18 @@ print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSV
 NuSVC_classifier = SklearnClassifier(NuSVC())
 NuSVC_classifier.train(training_set)
 print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
-#======================================================================================================================================#
 
-# the next approach we will try, since the remaining "scores" are relatively close, is to use all algos at once; we will create another
-# classifier, and make the result of that classifier based on what the other algorithms said; similar to a voting system, so we will
-# just need an odd number of algorithms...
+#===============================================================================================================================#
+# section iii
 
-# combining classifier algorithms is a common technique, accomplished by creating a sort of voting system, where each algorithm gets one
-# vote, and the classification that has the most votes is the chosen one; to do this, we want our new classifier to act like a typical
-# nltk classifier, with all of the methods; so we just need to use object oriented programming and ensure to inherit from the nltk
-# classifier class.
+# the next approach we will try, since the remaining "scores" are relatively close, is to use all algos at once; we will create
+# another classifier, and make the result of that classifier based on what the other algorithms said; similar to a voting system,
+# so we will just need an odd number of algorithms...
+# combining classifier algorithms is a common technique, accomplished by creating a sort of voting system, where each algorithm
+# gets one vote, and the classification that has the most votes is the chosen one; to do this, we want our new classifier to act
+# like a typical nltk classifier, with all of the methods; so we just need to use object oriented programming and ensure to
+# inherit from the nltk classifier class.
+
 
 from nltk.classify import ClassifierI
 from statistics import mode
@@ -98,7 +106,7 @@ class VoteClassifier(ClassifierI):
 	# above: we are calling our class VoteClassifier and inheriting from nltk's ClassifierI;
 	# then, we take the list of classifiers passed to our class, and assign it to self._classifiers
 	
-	# next, we create a classift method and call it classify; later, we can invoke it as 'nltk.classify'
+	# next, we create a classification method and call it classify; later, we can invoke it as 'nltk.classify'
 	
 	def classify(self, features):
 		votes = []
@@ -114,10 +122,10 @@ class VoteClassifier(ClassifierI):
 			# - so after iteration is complete, we have the most popular vote,
 			#				which is the classification returned the most often
 			
-	# next, we add an additional method for a confidence measure;
-	# we simply tally the number of votes for the winning vote,
-	# and compare that to the number of votes for the losing vote,
-	# obviously, the greater the margin, the more confidence we can have in the decision
+	# next, we add an additional method for a confidence measure; we simply tally the number of votes for the winning vote,
+	# and compare that to the number of votes for the losing vote, obviously, the greater the margin, the more confidence we can
+  # have in the decision; for example, 3/5 votes for positive is weaker than 5/5 votes for positive, so we can use the actual
+  # ratio of votes as a confidence indicator
 	
 	def confidence(self, feature):
 		votes = []
@@ -128,46 +136,140 @@ class VoteClassifier(ClassifierI):
 		choice_votes = votes.count(mode(votes))
 		conf = choice_votes / len(votes)
 		return conf
-		
-		
-#======================================================================================================================================#		
-#		next, we will combine all the code together.
-
-		
-		
+  
+#===============================================================================================================================#
+# section iv
 
 
+import nltk
+import random
+from nltk.corpus import movie_reviews
+from nltk.classify.scikitlearn import SklearnClassifier
+import pickle
+
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+
+from nltk.classify import ClassifierI
+from statistics import mode
 
 
+class VoteClassifier(ClassifierI):
+    def __init__(self, *classifiers):
+        self._classifiers = classifiers
+
+    def classify(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+        return mode(votes)
+
+    def confidence(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes / len(votes)
+        return conf
+
+      
+      
+documents = [(list(movie_reviews.words(fileid)), category)
+            for category in movie_reviews.categories()
+            for fileid in movie_reviews.fileids(category)]
+
+random.shuffle(documents)
+
+all_words = []
+
+for w in movie_reviews.words():
+    all_words.append(w.lower())
+
+all_words = nltk.FreqDist(all_words)
+
+word_features = list(all_words.keys())[:3000]
+
+def find_features(document):
+    words = set(document)
+    features = {}
+    for w in word_features:
+        features[w] = (w in words)
+
+    return features
+
+## print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
+featuresets = [(find_features(rev), category) for (rev, category) in documents]
+
+training_set = featuresets[:1900]
+testing_set = featuresets[1900:]
+
+# instead of training this classification algo again, we simply access the pickled algo file we saved previously; this
+# process is shown below (the commented out section directly below is old code before pickled algo was saved)
+# if you need to review this topic, as it will be used extensively in the upcoming sections, please go back over the section
+# titled "08_saving_classifiers.py" and/or "09_sklearn_algos_pt1.py"
+## classifier = nltk.NaiveBayesClassifier.train(training_set)
+## print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
+## classifier.show_most_informative_features(15)
+
+# if you are running this in a virtual environment or in general if you are having problems reaching the pickled algo, you may
+# need to enter the absolute path instead of simply a relative path like "naivebayes.pickle"; absolute path example shown below:
+## classifier_f = open("C:/Users/your/path/to/naivebayes.pickle","rb")
+classifier_f = open("naivebayes.pickle","rb")
+classifier = pickle.load(classifier_f)
+classifier_f.close()
+
+print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
+classifier.show_most_informative_features(15)
+
+MNB_classifier = SklearnClassifier(MultinomialNB())
+MNB_classifier.train(training_set)
+print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
+
+BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
+BernoulliNB_classifier.train(training_set)
+print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
+
+LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+LogisticRegression_classifier.train(training_set)
+print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+
+SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
+SGDClassifier_classifier.train(training_set)
+print("SGDClassifier_classifier accuracy percent:", (nltk.classify.accuracy(SGDClassifier_classifier, testing_set))*100)
+
+## SVC_classifier = SklearnClassifier(SVC())
+## SVC_classifier.train(training_set)
+## print("SVC_classifier accuracy percent:", (nltk.classify.accuracy(SVC_classifier, testing_set))*100)
+
+LinearSVC_classifier = SklearnClassifier(LinearSVC())
+LinearSVC_classifier.train(training_set)
+print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
+
+NuSVC_classifier = SklearnClassifier(NuSVC())
+NuSVC_classifier.train(training_set)
+print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
 
 
+voted_classifier = VoteClassifier(classifier,
+                                  NuSVC_classifier,
+                                  LinearSVC_classifier,
+                                  SGDClassifier_classifier,
+                                  MNB_classifier,
+                                  BernoulliNB_classifier,
+                                  LogisticRegression_classifier)
 
+print("voted_classifier accuracy percent:", (nltk.classify.accuracy(voted_classifier, testing_set))*100)
 
+print("Classification:", voted_classifier.classify(testing_set[0][0]), "Confidence %:", voted_classifier.confidence(testing_set[0][0])*100)
+print("Classification:", voted_classifier.classify(testing_set[1][0]), "Confidence %:", voted_classifier.confidence(testing_set[1][0])*100)
+print("Classification:", voted_classifier.classify(testing_set[2][0]), "Confidence %:", voted_classifier.confidence(testing_set[2][0])*100)
+print("Classification:", voted_classifier.classify(testing_set[3][0]), "Confidence %:", voted_classifier.confidence(testing_set[3][0])*100)
+print("Classification:", voted_classifier.classify(testing_set[4][0]), "Confidence %:", voted_classifier.confidence(testing_set[4][0])*100)
+print("Classification:", voted_classifier.classify(testing_set[5][0]), "Confidence %:", voted_classifier.confidence(testing_set[5][0])*100)		
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#===============================================================================================================================#
 
