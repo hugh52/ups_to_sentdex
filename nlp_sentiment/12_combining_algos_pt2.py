229e5709eb0851fd3586bfe5f78278c10a01d738
hugh52 <whcherry52@gmail.com>
2019-08-07 13:46:16 -0500

    Update 12_combining_algos_pt2.py

--- nlp_sentiment/12_combining_algos_pt2.py
+++ nlp_sentiment/12_combining_algos_pt2.py
@@ -89,14 +89,14 @@ for p in short_neg.split('\n'):
         if w[1][0] in allowed_word_types:
             all_words.append(w[0].lower())
 
-save_documents = open("nlp/pickled_algos_new/documents.pickle","wb")
+save_documents = open("data_etc/documents.pickle","wb")
 pickle.dump(documents, save_documents)
 save_documents.close()
 
 all_words = nltk.FreqDist(all_words)
 word_features = list(all_words.keys())[:5000]
 
-save_word_features = open("nlp/pickled_algos_new/word_features5k.pickle","wb")
+save_word_features = open("data_etc/word_features5k.pickle","wb")
 pickle.dump(word_features, save_word_features)
 save_word_features.close()
 
@@ -111,7 +111,7 @@ def find_features(document):
 featuresets = [(find_features(rev), category) for (rev, category) in documents]
 random.shuffle(featuresets)
 
-featuresets_save = open("nlp/pickled_algos_new/featuresets.pickle","wb")
+featuresets_save = open("data_etc/featuresets.pickle","wb")
 pickle.dump(featuresets, featuresets_save)
 featuresets_save.close()
 
@@ -120,6 +120,7 @@ testing_set = featuresets[10000:]
 
 
 # now we save all the classification algos as serialized (pickled) objects (files)
+# note that below we assume we are saving to a directory named 'data_etc/', the name should be updated for personal use. 
 
 # naive bayes
 
@@ -127,7 +128,7 @@ classifier = nltk.NaiveBayesClassifier.train(training_set)
 print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
 classifier.show_most_informative_features(15)
 
-save_classifier = open("nlp/pickled_algos_new/originalnaivebayes5k.pickle","wb")
+save_classifier = open("data_etc/originalnaivebayes5k.pickle","wb")
 pickle.dump(classifier, save_classifier)
 save_classifier.close()
 
@@ -137,7 +138,7 @@ MNB_classifier = SklearnClassifier(MultinomialNB())
 MNB_classifier.train(training_set)
 print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
 
-save_classifier = open("nlp/pickled_algos_new/MNB_classifier5k.pickle","wb")
+save_classifier = open("data_etc/MNB_classifier5k.pickle","wb")
 pickle.dump(MNB_classifier, save_classifier)
 save_classifier.close()
 
@@ -146,7 +147,7 @@ BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
 BernoulliNB_classifier.train(training_set)
 print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
 
-save_classifier = open("nlp/pickled_algos_new/BernoulliNB_classifier5k.pickle","wb")
+save_classifier = open("data_etc/BernoulliNB_classifier5k.pickle","wb")
 pickle.dump(BernoulliNB_classifier, save_classifier)
 save_classifier.close()
 
@@ -156,7 +157,7 @@ LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
 LogisticRegression_classifier.train(training_set)
 print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
 
-save_classifier = open("C:/Users/William/nlp/ghub/g_pickled_algos/pickled_algo_LogisticRegression_classifier5k.pickle","wb")
+save_classifier = open("data_etc/pickled_algo_LogisticRegression_classifier5k.pickle","wb")
 pickle.dump(LogisticRegression_classifier, save_classifier)
 save_classifier.close()
 
@@ -165,7 +166,7 @@ LinearSVC_classifier = SklearnClassifier(LinearSVC())
 LinearSVC_classifier.train(training_set)
 print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
 
-save_classifier = open("C:/Users/William/nlp/ghub/g_pickled_algos/pickled_algo_LinearSVC_classifier5k.pickle","wb")
+save_classifier = open("data_etc/pickled_algo_LinearSVC_classifier5k.pickle","wb")
 pickle.dump(LinearSVC_classifier, save_classifier)
 save_classifier.close()
 
@@ -179,6 +180,6 @@ SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
 SGDClassifier_classifier.train(training_set)
 print("SGDClassifier_classifier accuracy percent:", (nltk.classify.accuracy(SGDClassifier_classifier, testing_set))*100)
 
-save_classifier = open("C:/Users/William/nlp/ghub/g_pickled_algos/pickled_algo_SGDC_classifier5k.pickle","wb")
+save_classifier = open("data_etc/pickled_algo_SGDC_classifier5k.pickle","wb")
 pickle.dump(SGDClassifier_classifier, save_classifier)
 save_classifier.close()
